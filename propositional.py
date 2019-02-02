98929aa44ea6471949520363c35c4cf3960fee39
Giovanni Berti <dev.giovanniberti@gmail.com>
2019-02-02 18:56:36 +0100

    feat(propositional): add forward chaining algorithm for propositional logic

--- propositional.py
+++ propositional.py
@@ -1,6 +1,36 @@
-from primitives import Clause, Var, KB
+from collections import defaultdict
+
+from primitives import Clause, Var, HornKB, HornClause
 
 
 def unit_resolution(clause: Clause, literal: Var) -> Clause:
     """Unit resolution rule."""
     return clause - ~literal
+
+
+def forward_chaining_solve(knowledge_base: HornKB, query: Var) -> bool:
+    """Propositional logic entailment check with forward chaining.
+    (Answers to question: knowledge_base ⊨? query)"""
+
+    knowledge_base = HornKB(knowledge_base.clauses)
+    count = {clause: len(clause.body) for clause in knowledge_base}
+    inferred = defaultdict(bool)
+    knowns = [clause.head for clause in knowledge_base]
+
+    while knowns:
+        p = knowns.pop()
+
+        if p == query:
+            return True
+        if not inferred[p]:
+            inferred[p] = True
+
+            clauses_with_p = [clause for clause in knowledge_base if p in clause]
+
+            for clause in clauses_with_p:
+                count[clause] -= 1
+
+                if count[clause] == 0:
+                    knowns += [HornClause.from_clause(clause).head]
+
+    return False
