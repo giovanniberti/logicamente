03f363e5e08245a0d41e9b0b123c510332476433
Giovanni Berti <dev.giovanniberti@gmail.com>
2019-02-02 13:07:48 +0100

    test(*): add initial unit tests

---
+++ tests.py
@@ -0,0 +1,40 @@
+import unittest
+from primitives import Var, PrimitiveClause
+
+
+class PropositionalLogic(unittest.TestCase):
+
+    def test_var_eval(self):
+        a = Var("a", True)
+
+        self.assertEqual(a(), True)
+
+    def test_var_negate(self):
+        a = Var("a", True)
+
+        self.assertEqual((~a)(), False)
+
+    def test_primitive_clause_contains(self):
+        a = Var("a")
+        b = Var("b")
+
+        p = PrimitiveClause({a, b})
+
+        self.assertEqual(a in p, True)
+
+    def test_primitive_clause_remove(self):
+        a = Var("a")
+        b = Var("b")
+
+        p = PrimitiveClause({a, b})
+        p2 = PrimitiveClause({a})
+
+        self.assertEqual(p - a, p2)
+
+    def test_primitive_clause_eval(self):
+        a = Var("a", False)
+        b = Var("b", True)
+
+        p = PrimitiveClause({a, b})
+
+        self.assertEqual(p(), True)
