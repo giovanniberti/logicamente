76db0780f71243638c9b3ce281c7f6a6a00ad475
Giovanni Berti <dev.giovanniberti@gmail.com>
2019-02-05 15:54:13 +0100

    feat(first_order): add first order logic (FoL) classes

--- first_order.py
+++ first_order.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from primitives import Literal
+
+from primitives import Literal, Clause, Term
 
 
 @dataclass
@@ -7,17 +8,43 @@ class Relation:
     name: str
 
     def __call__(self, *args, **kwargs):
-        if len(args) != 2:
-            raise RuntimeError("wrong number of arguments in relation (two arguments required)")
+        if len(args) > 3:
+            raise RuntimeError("wrong number of arguments in relation (three args max)")
 
-        return RelationInstance(self.name, args[0], args[1])
+        if len(args) == 2:
+            return RelationInstance(self.name, args[0], args[1])
+        elif len(args) == 3:
+            return RelationInstance(self.name, args[0], args[1], args[2])
 
 
-@dataclass
-class RelationInstance:
+class RelationInstance(Term):
     relation_name: str
-    var1: Literal
-    var2: Literal
+    var1: Term
+    var2: Term
+
+    def __init__(self, relation_name: str, var1: Term, var2: Term, negate: bool = False):
+        super().__init__(negate)
+        object.__setattr__(self, "relation_name", relation_name)
+        object.__setattr__(self, "var1", var1)
+        object.__setattr__(self, "var2", var2)
+
+    def __contains__(self, item: Literal):
+        return item in self.var1 or item in self.var2
+
+    def __repr__(self):
+        return f"RelationInstance{{name={self.relation_name}, var1={self.var1}, var2={self.var2}, " \
+               f"negate={self.negate}}} "
+
+    def __str__(self):
+        string = ""
+
+        if self.negate:
+            string += "¬"
+
+        return string + f"{self.relation_name}({self.var1}, {self.var2})"
+
+    def __invert__(self):
+        return RelationInstance(self.relation_name, self.var1, self.var2, not self.negate)
 
 
 @dataclass
@@ -31,7 +58,83 @@ class Function:
         return FunctionInstance(self.name, args[0])
 
 
-@dataclass
-class FunctionInstance:
+class Var(Term):
+    name: str
+
+    def __init__(self, name: str, negate: bool = False):
+        super().__init__(negate)
+        object.__setattr__(self, "name", name)
+
+    def __repr__(self):
+        return f"Var{{name={self.name}, negate={self.negate}}}"
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __eq__(self, other):
+        return type(other) is Var and self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name) ^ hash(self.negate) ^ hash("Var")
+
+    def __contains__(self, item):
+        return type(item) is Var and self.name == item.name
+
+    def __invert__(self):
+        return Var(self.name, not self.negate)
+
+
+class FunctionInstance(Term):
     function_name: str
-    arg: object
+    arg: Term
+
+    def __init__(self, function_name, arg, negate=False):
+        super().__init__(negate)
+        object.__setattr__(self, "function_name", function_name)
+        object.__setattr__(self, "arg", arg)
+
+
+@dataclass(init=False, frozen=True)
+class Quantifier(Term):
+    variable: Var
+    predicate: Clause
+
+    def __init__(self, variable: Var, predicate: Clause, negate: bool = False):
+        super().__init__(negate)
+        object.__setattr__(self, "variable", variable)
+        object.__setattr__(self, "predicate", predicate)
+
+        if type(variable) is not Var:
+            raise TypeError(f"You must use `Var`s for variables. Actual type: {type(variable)}")
+
+        if variable not in predicate:
+            raise TypeError("Quantifier variable not present in predicate")
+
+    def __contains__(self, item):
+        return item in self.predicate
+
+
+class Exists(Quantifier):
+    def __str__(self):
+        string = "("
+
+        if self.negate:
+            string += "¬"
+
+        return string + f"∃ {self.variable.name} {self.predicate})"
+
+    def __invert__(self):
+        return Exists(self.variable, self.predicate, not self.negate)
+
+
+class ForAll(Quantifier):
+    def __str__(self):
+        string = "("
+
+        if self.negate:
+            string += "¬"
+
+        return string + f"∀ {self.variable.name} {self.predicate})"
+
+    def __invert__(self):
+        return ForAll(self.variable, self.predicate, not self.negate)
